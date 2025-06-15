fb31fdc7d58dfabfd4751e251c89969544007cea
BabatenkoTO <tanya-mongush@mail.ru>
2025-06-15 15:40:39 +0700

    add new change

--- src/main/java/praktikum/EnvConfig.java
+++ src/main/java/praktikum/EnvConfig.java
@@ -5,7 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.http.ContentType.JSON;
 
 public class EnvConfig {
-        public static final String BASE_URL= "https://stellarburgers.nomoreparties.site/";
+    public static final String BASE_URL= "https://stellarburgers.nomoreparties.site/";
 
     public static RequestSpecification getBaseSpec() {
         return new RequestSpecBuilder()
---
+++ src/main/java/praktikum/EnvURL.java
@@ -0,0 +1,9 @@
+package praktikum;
+
+public class EnvURL {
+    public static final String USER = "/api/auth/user";
+    public static final String ORDERS = "/api/orders";
+    public static final String INGREDIENTS = "/api/ingredients";
+    public static final String USER_REGISTER = "/api/auth/register";
+    public static final String USER_LOGIN = "/api/auth/login";
+}
